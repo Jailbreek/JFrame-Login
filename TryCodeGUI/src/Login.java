622159a62d3741ecc5f404744debd620855e822a
cpt_n3m0 <90511215+Jailbreek@users.noreply.github.com>
2023-03-25 02:10:21 +0700

    Add files via upload

--- TryCodeGUI/src/Login.java
+++ TryCodeGUI/src/Login.java
@@ -1,14 +1,13 @@
 import java.awt.EventQueue;
-
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import java.awt.Font;
 import java.awt.HeadlessException;
-
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JTextField;
+import javax.swing.SwingUtilities;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.io.BufferedReader;
@@ -16,24 +15,28 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.awt.event.ActionEvent;
 import java.awt.Color;
+import java.awt.Component;
+import javax.swing.JPasswordField;
+import javax.swing.JCheckBox;
 
 public class Login extends JFrame {
 
 	/**
 	 * 
 	 */
+	
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 	private JTextField usertxt;
-	private JTextField passtxt;
-	private JFrame Login;
-
+	private JPasswordField passtxt;
+	
 	/**
 	 * Launch the application.
 	 */
 
 	
-	public void login() throws IOException{ //Login Method & validation
+	@SuppressWarnings("deprecation")
+	public void login() throws IOException{
 		
 		String file = "src/data/Admin.txt";
 		
@@ -44,10 +47,8 @@ public class Login extends JFrame {
 			    String data[] = record.split(",");
 			    if (usertxt.getText().equals(data[0]) && passtxt.getText().equals(data[1])){
 			        JOptionPane.showMessageDialog(null, "Login Success \n Welcome " + usertxt.getText());
-//			        login = true;
-//                AdminMenu prod = new AdminMenu(usertxt.getText()); //calling product form
-//                prod.setVisible(true); //show the product form
-//                this.dispose(); //close the login form
+			        login = true;
+			        // U can continue with another JFrame 
 			    }
 			}
 			if (usertxt.getText().equals("")) {
@@ -80,6 +81,7 @@ public class Login extends JFrame {
 	/**
 	 * Create the frame.
 	 */
+	
 	public Login() {
 		setTitle("Login");
 		setFont(new Font("JetBrains Mono Medium", Font.PLAIN, 12));
@@ -107,18 +109,12 @@ public class Login extends JFrame {
 		contentPane.add(usertxt);
 		usertxt.setColumns(10);
 		
-		passtxt = new JTextField();
-		passtxt.setFont(new Font("JetBrains Mono", Font.PLAIN, 14));
-		passtxt.setColumns(10);
-		passtxt.setBounds(168, 135, 217, 23);
-		contentPane.add(passtxt);
-		
 		JButton btnLogin = new JButton("Login");
 		btnLogin.setFont(new Font("JetBrains Mono Medium", Font.PLAIN, 13));
 		btnLogin.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				try {
-					login(); //Calling login method
+					login();
 				} catch (IOException ex) {
 					ex.printStackTrace();
 				}
@@ -130,10 +126,10 @@ public class Login extends JFrame {
 		JButton btnRegister = new JButton("Register");
 		btnRegister.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				Register reg = new Register(); //calling regist form
-                reg.setVisible(true); //show the regist form
-                Login.dispose(); //close the login form
-				
+				Register log = new Register(); //calling login form
+                log.setVisible(true); //show the login form
+                JFrame frame = (JFrame) SwingUtilities.getWindowAncestor((Component)e.getSource());
+                frame.dispose();//Close Login Form
 			}
 		});
 		btnRegister.setFont(new Font("JetBrains Mono Medium", Font.PLAIN, 13));
@@ -148,5 +144,23 @@ public class Login extends JFrame {
 		JLabel tittle = new JLabel("Rumah Makan Borneo");
 		panel.add(tittle);
 		tittle.setFont(new Font("JetBrains Mono Medium", Font.PLAIN, 28));
+		
+		passtxt = new JPasswordField();
+		passtxt.setFont(new Font("JetBrains Mono", Font.PLAIN, 14));
+		passtxt.setBounds(168, 133, 217, 23);
+		contentPane.add(passtxt);
+		
+		JCheckBox checkbox = new JCheckBox("");
+		checkbox.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if (checkbox.isSelected()) {
+		            passtxt.setEchoChar((char) 0);
+		        } else {
+		        	passtxt.setEchoChar('*');
+		        }
+			}
+		});
+		checkbox.setBounds(368, 163, 21, 23);
+		contentPane.add(checkbox);
 	}
 }
